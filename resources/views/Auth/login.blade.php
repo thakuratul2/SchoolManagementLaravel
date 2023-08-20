f451eb3f4296c47d941faa04f203fdf90a31248a
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-20 11:16:17 +0530

    Auth Page Update

---
+++ resources/views/Auth/login.blade.php
@@ -0,0 +1,1 @@
+@extends('Auth.layouts.main')
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[AuthController::class,'login']);
 
 Route::get('admin-home',function(){
 
