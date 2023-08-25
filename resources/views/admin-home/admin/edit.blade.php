0beb39bcc9b81770f60a52e48096d85c5cb3ee1b
thakuratul2 <pratapsinghatul111@gmail.com>
2023-08-25 20:23:42 +0530

    Fix

--- resources/views/admin-home/admin/edit.blade.php
+++ resources/views/admin-home/admin/edit.blade.php
@@ -38,6 +38,7 @@
                   </div>
                   <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
+                    
                     <input type="password" name="pasword" class="form-control" placeholder="Enter Password Here: ">
                   </div>
               
