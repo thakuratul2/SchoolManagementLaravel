8d68fb4cc0848d15e24af49b8eea3920266c9ad2
thakuratul2 <pratapsinghatul111@gmail.com>
2023-09-04 21:01:25 +0530

    form done

--- resources/views/admin-home/student_details/addStudent.blade.php
+++ resources/views/admin-home/student_details/addStudent.blade.php
@@ -51,24 +51,101 @@
                       </div>
                       <div class="form-group col-md-6">
                     
-                        <label >Enter Last Name <span style="color: red;">*</span></label>
-                        <input type="text" name="last_name" name="{{old('last_name')}}" required class="form-control" placeholder="Enter Last Name Here: ">
-                      </div>
+                        <label >Class <span style="color: red;">*</span></label>
+
+                        <select name="class_id" class="form-control">
+                            <option value="">Select Class</option>
+                        </select>
                     </div>
+                    <div class="form-group col-md-6">
                     
+                        <label >Gender <span style="color: red;">*</span></label>
+
+                        <select name="gender" class="form-control">
+                            <option value="">Select Gender</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="Other">Other</option>
+                        </select>
                     </div>
+                    <div class="form-group col-md-6">
                     
+                        <label >Date Of Birth <span style="color: red;">*</span></label>
+
+                        <input type="date" class="form-control" required value="{{old('date_of_birth')}}" name="date_of_birth">
                     </div>
+                  
+
+                    <div class="form-group col-md-6">
+                    
+                        <label >Caste <span style="color: red;">*</span></label>
+
+                        <input type="text" class="form-control" required value="{{old('caste')}}" name="caste" placeholder="Enter your caste">
+                    </div>
+                    <div class="form-group col-md-6">
+                    
+                        <label >Religion <span style="color: red;">*</span></label>
+
+                        <input type="text" class="form-control" required value="{{old('religion')}}" name="religion" placeholder="Enter your religion">
+                    </div>
+                    <div class="form-group col-md-6">
+                    
+                        <label >Mobile Number <span style="color: red;">*</span></label>
+
+                        <input type="text" class="form-control" required value="{{old('mobile_number')}}" name="mobile_number" placeholder="Enter Number">
+                    </div>
+                    <div class="form-group col-md-6">
+                    
+                        <label >Admission Date <span style="color: red;">*</span></label>
+
+                        <input type="date" class="form-control" required value="{{old('admission_date')}}" name="admission_date">
+                    </div>
+                    <div class="form-group col-md-6">
+                    
+                        <label >Profile Pic <span style="color: red;">*</span></label>
+
+                        <input type="file" class="form-control" required value="{{old('profile_pic')}}" name="profile_pic">
+                    </div>
+                    <div class="form-group col-md-6">
+                    
+                        <label >Blood Group <span style="color: red;">*</span></label>
+
+                        <input type="text" class="form-control" required value="{{old('blood_group')}}" name="blood_group" placeholder="Blood Group">
+                    </div>
+                    <div class="form-group col-md-6">
+                    
+                        <label >Height <span style="color: red;">*</span></label>
+
+                        <input type="text" class="form-control" required value="{{old('height')}}" name="height" placeholder="Enter Height"> 
+                    </div>
+                    <div class="form-group col-md-6">
+                    
+                        <label >Weight <span style="color: red;">*</span></label>
+
+                        <input type="text" class="form-control" required value="{{old('weight')}}" name="weight" placeholder="Enter Weight">
+                    </div>
+                    <div class="form-group col-md-6">
+                    
+                        <label >Status <span style="color: red;">*</span></label>
+
+                        <select name="gender" class="form-control">
+                            <option value="">Select Status</option>
+                            <option value="0">Active</option>
+                            <option value="1">Inactive</option>
+                            
+                        </select>
+                    </div>
+                
                     
                     </div>
                     
                   <div class="form-group">
-                    <label >Enter Email</label>
-                    <input type="email" name="email" required class="form-control" placeholder="Enter Email Here: ">
+                    <label >Enter Email <span style="color: red;">*</span></label>
+                    <input type="email" name="email" value="{{old('email')}}" class="form-control" placeholder="Enter Email Here: ">
                   </div>
                   <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" name="pasword" required class="form-control" placeholder="Enter Password Here: ">
+                    <label>Password <span style="color: red;">*</span></label>
+                    <input type="password" name="pasword" value="{{old('password')}}" required class="form-control" placeholder="Enter Password Here: ">
                   </div>
               
                   
